be04ecaef3c48df54c8148021b9eef890e55af3e
Tri.Tran <tri.tran@vietunion.com.vn>
2023-10-17 13:30:40 +0700

    fix: config lsp-zero

--- nvim/plugin/lsp.rc.lua
+++ nvim/plugin/lsp.rc.lua
@@ -8,11 +8,9 @@ lsp_zero.on_attach(function(client, bufnr)
 
   vim.keymap.set("n", "<leader>ws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>rf", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set({ 'n', 'x' }, "<leader>fm", function() vim.lsp.buf.format({ async = false, timeout_ms = 10000 }) end,
     opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 lsp_zero.format_on_save({
