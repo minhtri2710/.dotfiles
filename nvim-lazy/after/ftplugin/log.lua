6a231b0c0520642fb65c75f47a3ed9e44ca5fe02
Tri.Tran <tri.tran@vietunion.com.vn>
2023-11-15 17:28:44 +0700

    feat: update config lazyvim

---
+++ nvim-lazy/after/ftplugin/log.lua
@@ -0,0 +1,3 @@
+vim.opt.filetype = "log" -- set filetype
+vim.opt.wrap = true
+vim.cmd(':set syntax=messages')
---
+++ nvim-lazy/lua/config/keymaps.lua
@@ -0,0 +1,73 @@
+local discipline = require("beowulf.discipline")
+
+discipline.cowboy()
+
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+keymap.set("n", "x", '"_x')
+
+-- Increment/decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- Delete a word backwards
+keymap.set("n", "dw", 'vb"_d')
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Disable continuations
+keymap.set("n", "<Leader>o", "o<Esc>^Da", opts)
+keymap.set("n", "<Leader>O", "O<Esc>^Da", opts)
+
+-- Jumplist
+keymap.set("n", "<C-m>", "<C-i>", opts)
+
+-- New tab
+keymap.set("n", "te", ":tabedit<Return>")
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+-- Split window
+keymap.set("n", "ss", ":split<Return>", opts)
+keymap.set("n", "sv", ":vsplit<Return>", opts)
+-- Move window
+keymap.set("n", "sh", "<C-w>h")
+keymap.set("n", "sk", "<C-w>k")
+keymap.set("n", "sj", "<C-w>j")
+keymap.set("n", "sl", "<C-w>l")
+
+-- Resize window
+keymap.set("n", "<C-w><left>", "<C-w><")
+keymap.set("n", "<C-w><right>", "<C-w>>")
+keymap.set("n", "<C-w><up>", "<C-w>+")
+keymap.set("n", "<C-w><down>", "<C-w>-")
+
+-- Keep center when up-down
+keymap.set("n", "j", "jzz")
+keymap.set("n", "k", "kzz")
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
+keymap.set("n", "n", "nzzzv")
+keymap.set("n", "N", "Nzzzv")
+
+keymap.set("n", "J", "mzJ`z")
+
+-- Move lines and group lines left, right
+keymap.set("v", ">", ">gv")
+keymap.set("v", "<", "<gv")
+
+-- Move lines and group lines up, down
+keymap.set("v", "K", ":m '<-2<cr>gv=gv")
+keymap.set("v", "J", ":m '>+1<cr>gv=gv")
+
+keymap.set("n", "<leader>r", function()
+  require("beowulf.utils").replaceHexWithHSL()
+end)
+
+-- greatest remap ever
+keymap.set("x", "<leader>p", [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+keymap.set("n", "<leader>Y", [["+Y]])
