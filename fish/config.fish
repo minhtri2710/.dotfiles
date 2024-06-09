9aad2515f24f4b69966c4fad37b192e8f4aa481f
Tri.Tran <inpro2710@gmail.com>
2024-06-09 12:29:22 +0700

    feat: update fish and nvim

--- fish/config.fish
+++ fish/config.fish
@@ -3,33 +3,26 @@ set fish_greeting
 set -gx TERM xterm-256color
 
 # aliases
-alias grep="grep -n --color "
-alias fgrep="fgrep -n --color "
+alias grep "grep -n --color "
+alias fgrep "fgrep -n --color "
 alias cls clear
 alias g git
-alias mux=tmuxinator
-alias :q=exit
-alias edge="open -a Microsoft\ Edge $1"
-command -qv nvim && alias vim nvim
-command -qv lazygit && alias lg lazygit
+alias mux tmuxinator
+alias :q exit
+alias lg lazygit
+alias vim nvim
+alias ls "eza --color=always -l --icons=always"
+alias la "ls -a"
+alias ll "ls -g"
+alias l1 "ls -g -1 "
+alias lla "ll -a"
 
 set -gx EDITOR nvim
 
-set -gx PATH $PATH /usr/local/opt/llvm/bin
-
-if type -q eza
-    alias ls="eza --color=always --long --icons=always"
-    alias ll="ls -G -l "
-    alias l1="ls -G -1 "
-    alias ls "ls -p -G"
-    alias la "ls -A"
-    alias lla "ll -A"
-end
-
-set LOCAL_CONFIG (dirname (status --current-filename))/config-local.fish
-if test -f $LOCAL_CONFIG
-    source $LOCAL_CONFIG
-end
+set -gx PATH bin $PATH
+set -gx PATH ~/bin $PATH
+set -gx PATH ~/.local/bin $PATH
+set -gx PATH node_modules/.bin $PATH
 
 #Starship
 starship init fish | source
